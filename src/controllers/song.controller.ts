673abeac69ed0a7375fdc794f729f8106f18c7b0
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-10-18 04:10:25 -0400

    feat: adding updatesong controller and route

--- src/controllers/song.controller.ts
+++ src/controllers/song.controller.ts
@@ -33,3 +33,14 @@ export const getSongById = async (req: Request, res: Response) => {
   }
 };
 
+export const updateSong = async (req: Request, res: Response) => {
+  try {
+    const song = await Song.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!song) {
+      return res.status(404).send();
+    }
+    res.status(200).send(song);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
--- src/routes/song.routes.ts
+++ src/routes/song.routes.ts
@@ -3,11 +3,13 @@ import {
   createSong,
   getSongs,
   getSongById,
+  updateSong,
 } from '../controllers/song.controller';
 const router: Router = Router();
 
 router.post('/', createSong);
 router.get('/', getSongs);
 router.get('/:id', getSongById);
+router.put('/:id', updateSong);
 
 export default router;
