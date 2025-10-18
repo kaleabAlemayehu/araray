39b9393fef45cf77c16bf3e859a1b7cb01c75f77
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-10-18 04:02:54 -0400

    feat: adding songroute on the express route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import connectDB from './config/db';
+import songRoutes from './routes/song.routes';
+
 dotenv.config();
 
 const app: Express = express();
@@ -14,6 +16,8 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Song API!');
 });
+
+app.use('/songs', songRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
