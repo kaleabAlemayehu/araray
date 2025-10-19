f0242fdab7ec373ad7985d4dbe5ea18e2f48e3ec
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-10-19 14:36:25 -0400

    feat: adding cors support

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import connectDB from './config/db';
 import songRoutes from './routes/song.routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 
 connectDB();
@@ -17,7 +21,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Song API!');
 });
 
-app.use('/songs', songRoutes);
+app.use('/api/v1/songs', songRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
